5006f748568916599e43ac56fa335d4644bc084c
Damien Grebil <damien.grebil@epfedu.fr>
2024-09-20 17:58:16 +0200

    Exo 1 de la partie 3: Calculator

--- P3 exo1/src/p3/exo1/P3Exo1.java
+++ P3 exo1/src/p3/exo1/P3Exo1.java
@@ -5,6 +5,8 @@
  */
 package p3.exo1;
 
+import java.util.Scanner;
+
 /**
  *
  * @author damie
@@ -15,7 +17,15 @@ public class P3Exo1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        int operateur ;
+        int operante1; 
+        int operante2;
+        Scanner sc = new Scanner(System.in);
         System.out.println(" 1) add\n 2)substract\n 3)multiply\n 4)divide\n 5)modulo");
+        operateur = sc.nextInt();
+        System.out.println("Saisissez une premiere valeure: ");
+        operante1 = sc.nextInt();
+       System.out.println("Saisissez une deuxieme valeure: ");
+        operante2 = sc.nextInt();
     }
-    
 }
