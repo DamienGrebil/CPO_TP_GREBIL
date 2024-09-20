caa268544797af72ad832a1dea9a1830b7577e73
Damien Grebil <damien.grebil@epfedu.fr>
2024-09-20 17:47:21 +0200

    Exo 1 de la partie 3: Calculator

---
+++ P3 exo1/src/p3/exo1/P3Exo1.java
@@ -0,0 +1,21 @@
+/*
+ * TP0 P3 Calculator
+ * DamienGrebil
+ * Septembre 2024
+ */
+package p3.exo1;
+
+/**
+ *
+ * @author damie
+ */
+public class P3Exo1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println(" 1) add\n 2)substract\n 3)multiply\n 4)divide\n 5)modulo");
+    }
+    
+}
