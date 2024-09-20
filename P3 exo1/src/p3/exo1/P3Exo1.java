70fc58e1ecc38831024d74268b44576e2594e4d0
Damien Grebil <damien.grebil@epfedu.fr>
2024-09-20 18:20:53 +0200

    Exo 1 de la partie 3: Calculator

--- P3 exo1/src/p3/exo1/P3Exo1.java
+++ P3 exo1/src/p3/exo1/P3Exo1.java
@@ -20,12 +20,37 @@ public class P3Exo1 {
         int operateur ;
         int operante1; 
         int operante2;
+        int result;
         Scanner sc = new Scanner(System.in);
         System.out.println(" 1) add\n 2)substract\n 3)multiply\n 4)divide\n 5)modulo");
         operateur = sc.nextInt();
+        if ( operateur < 1||operateur > 5 ){
+            System.out.println("La valeure choisie doit etre entre 1 et 5");
+            System.exit(0);
+        }
         System.out.println("Saisissez une premiere valeure: ");
         operante1 = sc.nextInt();
-       System.out.println("Saisissez une deuxieme valeure: ");
+        System.out.println("Saisissez une deuxieme valeure: ");
         operante2 = sc.nextInt();
+        if (operateur == 1) {
+            result = operante1 + operante2;
+            System.out.println("Le resultat est: "+ result);
+        }
+        if (operateur == 2) {
+            result = operante1 - operante2;
+            System.out.println("Le resultat est: "+ result);
+        }
+        if (operateur == 3) {
+            result = operante1 * operante2;
+            System.out.println("Le resultat est: "+ result);
+        }
+        if (operateur == 4){
+            result = operante1 / operante2;
+            System.out.println("Le resultat est: "+ result);
+        }
+        if (operateur == 5) {
+            result = operante1 % operante2;
+            System.out.println("Le resultat est: "+ result);
+        }
     }
 }
