3a67968df7083130feab58df2abb4403346100e8
Damien Grebil <damien.grebil@epfedu.fr>
2024-09-20 17:37:47 +0200

    Exo 2

--- Exo 2/src/exo/pkg2/Exo2.java
+++ Exo 2/src/exo/pkg2/Exo2.java
@@ -5,6 +5,7 @@
  */
 package exo.pkg2;
 
+import java.util.Scanner;
 /**
  *
  * @author damien
@@ -16,15 +17,20 @@ public class Exo2 {
      */
     public static void main(String[] args) {
         //Declaration des variables
-        int nb; // nombre dentiers a additionner
         int result; // resultat
         int ind; //indice
-        nb = 5;
+        int nb;
+        //nb=5;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("\n Entrer le nombre :");
+        nb = sc.nextInt(); // On demande a sc de donner le prochain entier
+
         result = 0;
         // Addition des nb premiers entiers
         ind = 1;
         while (ind <= nb) {
             result = result + ind;
+            ind += 1;
         }
         // Affichage du resultat
         System.out.println();
