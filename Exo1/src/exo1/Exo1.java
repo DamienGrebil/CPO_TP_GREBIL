cc4ac55dc492acbae7fcfa05b7178c06f490f2b1
Damien Grebil <damien.grebil@epfedu.fr>
2024-09-20 16:51:54 +0200

    Exo 1

--- Exo1/src/exo1/Exo1.java
+++ Exo1/src/exo1/Exo1.java
@@ -1,12 +1,15 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ * TP0 Exercice 1
+ * DamienGrebil
+ * Septembre 2024
  */
 package exo1;
 
+import java.util.Scanner;
+
 /**
  *
- * @author damie
+ * @author damien
  */
 public class Exo1 {
 
@@ -14,7 +17,15 @@ public class Exo1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        System.out.println("Bonjour");
+        String prenom;
+        Scanner sc;
+        sc = new Scanner(System.in);
+        System.out.println("Quel est votre prenom ?");
+        prenom = sc.nextLine();
+        System.out.println("Bye");
+        
     }
     
 }
