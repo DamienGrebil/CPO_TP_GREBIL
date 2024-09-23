57e25d5fe3964fa8b61d8813f173bd166f667a36
Damien Grebil <damien.grebil@epfedu.fr>
2024-09-23 12:46:46 +0200

    TP1 exo 2

--- TP1_convertisseur_DAMIEN/src/tp1_convertisseur_damien/TP1_convertisseur_DAMIEN.java
+++ TP1_convertisseur_DAMIEN/src/tp1_convertisseur_damien/TP1_convertisseur_DAMIEN.java
@@ -13,27 +13,80 @@ import java.util.Scanner;
  */
 public class TP1_convertisseur_DAMIEN {
     
-    public static double CelciusVersKelvin(double tCelcius) {
-        double val;
-        Scanner sc = new Scanner(System.in);
-        System.out.println("saisssissez une valeure reelle(la temperature)");
-        val = sc.nextDouble();
-        val += 273.15 ;
-        System.out.println("Voici la temperature en Kelvin: ");
-        return val;
+    public static double CelciusVersKelvin(double tKEL) {
+        tKEL += 273.15 ;
+        return tKEL ;
     }
     
+    public static double KelvinVersCelcius(double tCELC) {
+        tCELC -= 273.15 ;
+        return tCELC;
+    }
+    
+    public static double CelciusVersFarenheit(double tFAR) {
+        tFAR = (tFAR * 9/5) + 32;
+        return tFAR;
+    }
+    
+    public static double FarenheitVersCelcius(double tCEL) {
+        tCEL = (tCEL - 32) * 5/9;
+        return tCEL;
+    }
+    
+    public static double KelvinVersFarenheit(double tFARE ) {
+        tFARE = KelvinVersCelcius(tFARE);
+        tFARE = CelciusVersFarenheit(tFARE);
+        return tFARE;
+    }
+    
+    public static double FarenheitVersKelvin(double tKELV ) {
+        tKELV = KelvinVersCelcius(tKELV);
+        tKELV = CelciusVersFarenheit(tKELV);
+        return tKELV;
+    }
     
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        double val;
+        double temp;
+        double C2K ;
+        double C2F ;
+        double F2C ;
+        double F2K ;
+        double K2F ;
+        double K2C ;
+        int type;
         Scanner sc = new Scanner(System.in);
-        System.out.println("saisssissez une valeure reelle(la temperature)");
-        val = sc.nextDouble();
-        val += 273.15 ;
-        System.out.println("Voici la temperature en Kelvin: "+val);
+        System.out.println("saisssissez la temperature");
+        temp = sc.nextDouble();
+        System.out.println("Saisissez la conversion que vous souhaiter effectuer : \n 1|De Celcius vers Kelvin \n 2| De Kelvin vers Celcius \n 3| De Farenheit vers Celcius \n 4| De Celcius vers Farenheit \n 5| De Kelvin vers Farenheit \n 6| De Farenheit vers Kelvin  ");
+        type = sc.nextInt();
+        if (type == 1){
+            C2K = CelciusVersKelvin(temp);
+            System.out.println("la temperature en Kelvin est:" + C2K);
+        }
+        if (type == 2){
+            C2K = KelvinVersCelcius(temp);
+            System.out.println("la temperature en Celcius est:" + C2K);
+        }
+        if (type == 3){
+            C2K = FarenheitVersCelcius(temp);
+            System.out.println("la temperature en Celcius est:" + C2K);
+        }
+        if (type == 4){
+            C2K = CelciusVersFarenheit(temp);
+            System.out.println("la temperature en Kelvin est:" + C2K);
+        }
+        if (type == 5){
+            C2K = KelvinVersFarenheit(temp);
+            System.out.println("la temperature en Farenheit est:" + C2K);
+        }
+        if (type == 6){
+            C2K = FarenheitVersKelvin(temp);
+            System.out.println("la temperature en Kelvin est:" + C2K);
+        }
+        
     }
     
 }
