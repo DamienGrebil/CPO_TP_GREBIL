8fa02d8d25c1fc5316f8dd4fb44b357425d370d1
Damien Grebil <damien.grebil@epfedu.fr>
2024-09-23 12:04:23 +0200

    TP1 exo 2

--- TP1_convertisseur_DAMIEN/src/tp1_convertisseur_damien/TP1_convertisseur_DAMIEN.java
+++ TP1_convertisseur_DAMIEN/src/tp1_convertisseur_damien/TP1_convertisseur_DAMIEN.java
@@ -5,17 +5,35 @@
 */
 package tp1_convertisseur_damien;
 
+import java.util.Scanner;
+
 /**
  *
  * @author damie
  */
 public class TP1_convertisseur_DAMIEN {
-
+    
+    public static double CelciusVersKelvin(double tCelcius) {
+        double val;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("saisssissez une valeure reelle(la temperature)");
+        val = sc.nextDouble();
+        val += 273.15 ;
+        System.out.println("Voici la temperature en Kelvin: ");
+        return val;
+    }
+    
+    
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("saisssissez une valeure réelle");
+        double val;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("saisssissez une valeure reelle(la temperature)");
+        val = sc.nextDouble();
+        val += 273.15 ;
+        System.out.println("Voici la temperature en Kelvin: "+val);
     }
     
 }
