16d9b1c67745ac67a61ec485afaeb3cd2a394260
Damien Grebil <damien.grebil@epfedu.fr>
2024-09-23 11:55:17 +0200

    TP1 exo 2

---
+++ TP1_convertisseur_DAMIEN/src/tp1_convertisseur_damien/TP1_convertisseur_DAMIEN.java
@@ -0,0 +1,21 @@
+/*
+* TP1 convertisseur
+* DamienGrebil
+* Septembre 2024
+*/
+package tp1_convertisseur_damien;
+
+/**
+ *
+ * @author damie
+ */
+public class TP1_convertisseur_DAMIEN {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("saisssissez une valeure réelle");
+    }
+    
+}
