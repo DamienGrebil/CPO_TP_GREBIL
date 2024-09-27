e0c8fa971c4ae5fb584b61d77e985a5f95f8584b
Damien Grebil <damien.grebil@epfedu.fr>
2024-09-27 11:02:03 +0200

    TP1 exo 3

--- TP1_guessMyNumber_DAMIEN/src/tp1_guessmynumber_damien/TP1_guessMyNumber_DAMIEN.java
+++ TP1_guessMyNumber_DAMIEN/src/tp1_guessmynumber_damien/TP1_guessMyNumber_DAMIEN.java
@@ -2,7 +2,7 @@
 * TP1 P3 GuessMyNumber
 * DamienGrebil
 * Septembre 2024
-*/
+ */
 package tp1_guessmynumber_damien;
 
 import java.util.Random;
@@ -19,28 +19,60 @@ public class TP1_guessMyNumber_DAMIEN {
      */
     public static void main(String[] args) {
         int val;
-        int compt=1;
+        int compt = 1;
+        int jeu;
         Random generateurAleat = new Random();
         int n = generateurAleat.nextInt(100);
         Scanner sc = new Scanner(System.in);
         System.out.println(n);
+        System.out.println("Choississez le niveau de difficulté: \n1)facile \n2)normal \n3)difficile");
+        jeu = sc.nextInt();
         System.out.println("Choississez une valeure entre 0 et 100");
         val = sc.nextInt();
-        while (val != n) {
-            compt += 1;
-            if (val < n) {
-                System.out.println("Trop petit ");
-                System.out.println("Choississez une valeure entre 0 et 100");
-                val = sc.nextInt();
-            }else if (val > n) {
-                System.out.println("Trop grand ");
-                System.out.println("Choississez une valeure entre 0 et 100");
-                val = sc.nextInt();
-            }else if (val == n) {
-                break;
+        if (jeu == 1) {/*Mode facile*/
+            while (val != n) {
+                compt += 1;
+                if (val < n) {
+                    if (n - val < 10) {
+                        System.out.println("Trop petit ");
+                    }
+                    if (n - val > 10) {
+                        System.out.println("Trop petit mais pas loin ");
+                    }
+                    System.out.println("Choississez une valeure entre 0 et 100");
+                    val = sc.nextInt();
+                } else if (val > n) {
+                    if (val-n < 10) {
+                        System.out.println("Trop grand ");
+                    }
+                    if (val-n > 10) {
+                        System.out.println("Trop grand mais pas loin ");
+                    }
+                    System.out.println("Choississez une valeure entre 0 et 100");
+                    val = sc.nextInt();
+                } else if (val == n) {
+                    break;
+                }
             }
+            System.out.println("gagne, vous avez fait " + compt + " essais");
         }
-    System.out.println("gagne, vous avez fait "+ compt+" essais");
+        if (jeu == 2) {/*Mode normal*/
+            while (val != n) {
+                compt += 1;
+                if (val < n) {
+                    System.out.println("Trop petit ");
+                    System.out.println("Choississez une valeure entre 0 et 100");
+                    val = sc.nextInt();
+                } else if (val > n) {
+                    System.out.println("Trop grand ");
+                    System.out.println("Choississez une valeure entre 0 et 100");
+                    val = sc.nextInt();
+                } else if (val == n) {
+                    break;
+                }
+            }
+            System.out.println("gagne, vous avez fait " + compt + " essais");
+        }
     }
-    
+}
 }
