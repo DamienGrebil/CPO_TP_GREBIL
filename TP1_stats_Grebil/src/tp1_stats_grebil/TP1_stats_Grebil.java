db08b9eb7895299d590e4e36be02c33c6d6c2ccd
Damien Grebil <damien.grebil@epfedu.fr>
2024-09-28 11:57:35 +0200

    TP1 exo 4

--- TP1_stats_Grebil/src/tp1_stats_grebil/TP1_stats_Grebil.java
+++ TP1_stats_Grebil/src/tp1_stats_grebil/TP1_stats_Grebil.java
@@ -1,8 +1,12 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+* TP1 stats
+* DamienGrebil
+* Septembre 2024
+*/
 package tp1_stats_grebil;
+
+import java.util.Random;
+import java.util.Scanner;
 
 /**
  *
@@ -14,6 +18,16 @@ public class TP1_stats_Grebil {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        int m;
+        int [] tab =new int [6];
+        Scanner sc = new Scanner(System.in);
+        System.out.println("saississez un nombre entier");
+        m = sc.nextInt();
+        for (int i=0;i<m;i++){
+            Random generateurAleat = new Random();
+            tab[i] = generateurAleat.nextInt(6);
+            System.out.print(tab[i]+" ");
+        }
         
     }
     
