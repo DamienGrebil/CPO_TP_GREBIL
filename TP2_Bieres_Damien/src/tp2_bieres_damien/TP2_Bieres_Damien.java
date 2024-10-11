119746693fdf7bdfdc110e2d13865e419cd204fd
Damien Grebil <damien.grebil@epfedu.fr>
2024-10-11 08:12:52 +0200

    TP2 exo 1

---
+++ TP2_Bieres_Damien/src/tp2_bieres_damien/TP2_Bieres_Damien.java
@@ -0,0 +1,21 @@
+/*
+* TP2 bieres
+* DamienGrebil
+* Octobre 2024
+*/
+package tp2_bieres_damien;
+
+/**
+ *
+ * @author damie
+ */
+public class TP2_Bieres_Damien {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+    
+}
