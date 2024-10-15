0368bdf3636c1a29e54d7e1bb22b2638914d91a5
Damien Grebil <damien.grebil@epfedu.fr>
2024-10-15 11:13:36 +0200

    TP2 exo 2

---  TP2_convertisseurObjet_GREBIL/src/tp2_convertisseurobjet_grebil/TP2_convertisseurObjet_GREBIL.java
+++  TP2_convertisseurObjet_GREBIL/src/tp2_convertisseurobjet_grebil/TP2_convertisseurObjet_GREBIL.java
@@ -5,6 +5,8 @@
 */
 package tp2_convertisseurobjet_grebil;
 
+import java.util.Scanner;
+
 /**
  *
  * @author damie
@@ -18,6 +20,39 @@ public class TP2_convertisseurObjet_GREBIL {
         Convertisseur calcul = new Convertisseur();
         calcul.toString();
         System.out.println(calcul);
+    }
+    double temp;
+    double C2K;
+    int type;
+    Scanner sc = new Scanner(System.in);
+    System.out.println ("saisssissez la temperature");
+    temp  = sc.nextDouble();
+
+    System.out.println ("Saisissez la conversion que vous souhaiter effectuer : \n 1|De Celcius vers Kelvin \n 2| De Kelvin vers Celcius \n 3| De Farenheit vers Celcius \n 4| De Celcius vers Farenheit \n 5| De Kelvin vers Farenheit \n 6| De Farenheit vers Kelvin  ");
+    type  = sc.nextInt();
+    if (type == 1){
+        C2K = CelciusVersKelvin(temp);
+        System.out.println("la temperature en Kelvin est:" + C2K);
+    }
+    if (type== 2){
+        C2K = KelvinVersCelcius(temp);
+        System.out.println("la temperature en Celcius est:" + C2K);
+    }
+    if (type== 3){
+        C2K = FarenheitVersCelcius(temp);
+        System.out.println("la temperature en Celcius est:" + C2K);
+    }
+    if (type== 4){
+        C2K = CelciusVersFarenheit(temp);
+        System.out.println("la temperature en Kelvin est:" + C2K);
+    }
+    if (type== 5){
+        C2K = KelvinVersFarenheit(temp);
+        System.out.println("la temperature en Farenheit est:" + C2K);
+    }
+    if (type== 6){
+        C2K = FarenheitVersKelvin(temp);
+        System.out.println("la temperature en Kelvin est:" + C2K);
     }
     
 }
