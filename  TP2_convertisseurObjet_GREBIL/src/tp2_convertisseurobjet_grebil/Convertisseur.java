6d7e3e0946f21593ccb31d4843ecc6533fbd4a96
Damien Grebil <damien.grebil@epfedu.fr>
2024-10-15 11:13:44 +0200

    TP2 exo 2

---  TP2_convertisseurObjet_GREBIL/src/tp2_convertisseurobjet_grebil/Convertisseur.java
+++  TP2_convertisseurObjet_GREBIL/src/tp2_convertisseurobjet_grebil/Convertisseur.java
@@ -4,57 +4,56 @@ package tp2_convertisseurobjet_grebil;
 * TP2 bieres
 * DamienGrebil
 * Octobre 2024
-*/
-
-import java.util.Scanner;
+ */
 
 /**
  *
  * @author damie
  */
 public class Convertisseur {
-int nbConversions = 0;
+
+    int nbConversions = 0;
 
     public float CelciusVersKelvin(float tKEL) {
-        tKEL += 273.15 ;
-        nbConversions ++ ; // incremente le nombre de convertions
-        return tKEL ;
+        tKEL += 273.15;
+        nbConversions++; // incremente le nombre de convertions
+        return tKEL;
     }
-    
+
     public float KelvinVersCelcius(float tCELC) {
-        tCELC -= 273.15 ;
-        nbConversions ++ ; // incremente le nombre de convertions
+        tCELC -= 273.15;
+        nbConversions++; // incremente le nombre de convertions
         return tCELC;
     }
-    
+
     public float CelciusVersFarenheit(float tFAR) {
-        tFAR = (tFAR * 9/5) + 32;
-        nbConversions ++ ; // incremente le nombre de convertions
+        tFAR = (tFAR * 9 / 5) + 32;
+        nbConversions++; // incremente le nombre de convertions
         return tFAR;
     }
-    
+
     public float FarenheitVersCelcius(float tCEL) {
-        tCEL = (tCEL - 32) * 5/9;
-        nbConversions ++ ; // incremente le nombre de convertions
+        tCEL = (tCEL - 32) * 5 / 9;
+        nbConversions++; // incremente le nombre de convertions
         return tCEL;
     }
-    
-    public float KelvinVersFarenheit(float tFARE ) {
+
+    public float KelvinVersFarenheit(float tFARE) {
         tFARE = KelvinVersCelcius(tFARE);
         tFARE = CelciusVersFarenheit(tFARE);
-        nbConversions ++ ; // incremente le nombre de convertions
+        nbConversions++; // incremente le nombre de convertions
         return tFARE;
     }
-    
-    public float FarenheitVersKelvin(float tKELV ) {
+
+    public float FarenheitVersKelvin(float tKELV) {
         tKELV = FarenheitVersCelcius(tKELV);
         tKELV = CelciusVersKelvin(tKELV);
-        nbConversions ++ ; // incremente le nombre de convertions
+        nbConversions++; // incremente le nombre de convertions
         return tKELV;
     }
-    
-    @Override 
-    public String toString () { 
-    return "nb de conversions"+ nbConversions; 
-}
+
+    @Override
+    public String toString() {
+        return "nb de conversions =" + nbConversions;
+    }
 }
