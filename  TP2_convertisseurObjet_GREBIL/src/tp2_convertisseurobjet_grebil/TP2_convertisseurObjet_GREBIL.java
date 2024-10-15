366571980e498fb9e199c558ab6e6e423776f5ee
Damien Grebil <damien.grebil@epfedu.fr>
2024-10-15 10:56:48 +0200

    TP2 exo 2

---  TP2_convertisseurObjet_GREBIL/src/tp2_convertisseurobjet_grebil/TP2_convertisseurObjet_GREBIL.java
+++  TP2_convertisseurObjet_GREBIL/src/tp2_convertisseurobjet_grebil/TP2_convertisseurObjet_GREBIL.java
@@ -15,7 +15,7 @@ public class TP2_convertisseurObjet_GREBIL {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Convertisseur  = new Convertisseur();
     }
     
 }
