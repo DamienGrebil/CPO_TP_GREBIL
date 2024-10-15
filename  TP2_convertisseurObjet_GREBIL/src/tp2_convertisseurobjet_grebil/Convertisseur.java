c03475e45504ed83213335754d5125d51bca18bc
Damien Grebil <damien.grebil@epfedu.fr>
2024-10-15 10:57:04 +0200

    TP2 exo 2

---  TP2_convertisseurObjet_GREBIL/src/tp2_convertisseurobjet_grebil/Convertisseur.java
+++  TP2_convertisseurObjet_GREBIL/src/tp2_convertisseurobjet_grebil/Convertisseur.java
@@ -13,38 +13,42 @@ import java.util.Scanner;
  * @author damie
  */
 public class Convertisseur {
-int nbConvertions;
+int nbConversions = 0;
 
-    public static float CelciusVersKelvin(float tKEL) {
+    public float CelciusVersKelvin(float tKEL) {
         tKEL += 273.15 ;
         return tKEL ;
     }
     
-    public static float KelvinVersCelcius(float tCELC) {
+    public float KelvinVersCelcius(float tCELC) {
         tCELC -= 273.15 ;
         return tCELC;
     }
     
-    public static float CelciusVersFarenheit(float tFAR) {
+    public float CelciusVersFarenheit(float tFAR) {
         tFAR = (tFAR * 9/5) + 32;
         return tFAR;
     }
     
-    public static float FarenheitVersCelcius(float tCEL) {
+    public float FarenheitVersCelcius(float tCEL) {
         tCEL = (tCEL - 32) * 5/9;
         return tCEL;
     }
     
-    public static float KelvinVersFarenheit(float tFARE ) {
+    public float KelvinVersFarenheit(float tFARE ) {
         tFARE = KelvinVersCelcius(tFARE);
         tFARE = CelciusVersFarenheit(tFARE);
         return tFARE;
     }
     
-    public static float FarenheitVersKelvin(float tKELV ) {
+    public float FarenheitVersKelvin(float tKELV ) {
         tKELV = FarenheitVersCelcius(tKELV);
         tKELV = CelciusVersKelvin(tKELV);
         return tKELV;
     }
     
+    @Override 
+    public String toString () { 
+    return "nb de conversions"+ nbConversions; 
+}
 }
