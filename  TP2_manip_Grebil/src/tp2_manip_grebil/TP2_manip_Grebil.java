525b5bcb40cf769ae5f590fec2b4f0314471dfa0
Damien Grebil <damien.grebil@epfedu.fr>
2024-10-15 13:52:52 +0200

    TP2 exo 3

---  TP2_manip_Grebil/src/tp2_manip_grebil/TP2_manip_Grebil.java
+++  TP2_manip_Grebil/src/tp2_manip_grebil/TP2_manip_Grebil.java
@@ -16,7 +16,8 @@ public class TP2_manip_Grebil {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
+        Tartiflette assiette1 = new Tartiflette(500) ; 
+        Tartiflette assiette2 = new Tartiflette(600) ; 
     }
     
 }
