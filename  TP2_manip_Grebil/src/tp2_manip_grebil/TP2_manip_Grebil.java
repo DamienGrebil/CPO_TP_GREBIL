922c1d1eb5f1c02d4940f0b6aba16aea92359417
Damien Grebil <damien.grebil@epfedu.fr>
2024-10-15 13:43:24 +0200

    TP2 exo 2

---
+++  TP2_manip_Grebil/src/tp2_manip_grebil/TP2_manip_Grebil.java
@@ -0,0 +1,22 @@
+/*
+* TP2 Manip
+* DamienGrebil
+* Octobre 2024
+ */
+
+package tp2_manip_grebil;
+
+/**
+ *
+ * @author damie
+ */
+public class TP2_manip_Grebil {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+    
+}
