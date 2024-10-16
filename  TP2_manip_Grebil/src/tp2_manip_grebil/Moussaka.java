9878ad1caa56e1622d37d23773e1d2d60b60096d
Damien Grebil <damien.grebil@epfedu.fr>
2024-10-16 11:13:07 +0200

    TP2 exo 3

---  TP2_manip_Grebil/src/tp2_manip_grebil/Moussaka.java
+++  TP2_manip_Grebil/src/tp2_manip_grebil/Moussaka.java
@@ -10,11 +10,9 @@ package tp2_manip_grebil;
  * @author damie
  */
 public class Moussaka {
-    int nbCalories;
-    Moussaka[] tableauMoussaka = new Moussaka[10];
-    for( int i= 0; i<= 10;i++) {
-        Moussaka[10] = 
+int nbCalorie;
+    public Moussaka() {
+        Moussaka(nbCalorie);
     }
-    
-    
+       
 }
