62864905c1cf70584154691cd54289dcc9ec75dc
Damien Grebil <damien.grebil@epfedu.fr>
2024-10-16 10:45:17 +0200

    TP2 exo 2

---  TP2_convertisseurObjet_GREBIL/src/tp2_convertisseurobjet_grebil/Convertisseur.java
+++  TP2_convertisseurObjet_GREBIL/src/tp2_convertisseurobjet_grebil/Convertisseur.java
@@ -13,81 +13,46 @@ import java.util.Scanner;
  * @author damie
  */
 public class Convertisseur {
-
-    int nbConversions = 0;
-    double temp;
-    double C2K;
-    int type;
-
-    public float CelciusVersKelvin(float tKEL) {
-        tKEL += 273.15;
-        nbConversions++; // incremente le nombre de convertions
-        return tKEL;
+    int nbConversions;
+    public Convertisseur () {
+        nbConversions= 0 ;
     }
-
-    public float KelvinVersCelcius(float tCELC) {
-        tCELC -= 273.15;
-        nbConversions++; // incremente le nombre de convertions
-        return tCELC;
+      public double CelciusVersKelvin (double tCelcius) {
+        nbConversions+=1;
+        tCelcius=tCelcius+273.15;
+        return tCelcius;
     }
-
-    public float CelciusVersFarenheit(float tFAR) {
-        tFAR = (tFAR * 9 / 5) + 32;
-        nbConversions++; // incremente le nombre de convertions
-        return tFAR;
+    public double KelvinVersCelcius (double tKelvin){
+        nbConversions+=1;
+        tKelvin=tKelvin-273.15;
+        return tKelvin;    
     }
-
-    public float FarenheitVersCelcius(float tCEL) {
-        tCEL = (tCEL - 32) * 5 / 9;
-        nbConversions++; // incremente le nombre de convertions
-        return tCEL;
+    public double FarenheitVersCelcius (double tFar){
+        nbConversions+=1;
+        tFar =(tFar-32)/1.8;
+        return tFar;    
     }
-
-    public float KelvinVersFarenheit(float tFARE) {
-        tFARE = KelvinVersCelcius(tFARE);
-        tFARE = CelciusVersFarenheit(tFARE);
-        nbConversions++; // incremente le nombre de convertions
-        return tFARE;
+    public double CelciusVersFarenheit (double tCelcius){
+        nbConversions+=1;
+        tCelcius =tCelcius*1.8+32;
+        return tCelcius;
     }
-
-    public float FarenheitVersKelvin(float tKELV) {
-        tKELV = FarenheitVersCelcius(tKELV);
-        tKELV = CelciusVersKelvin(tKELV);
-        nbConversions++; // incremente le nombre de convertions
-        return tKELV;
+    public double KelvinVersFarenheit (double tKelvin){
+        nbConversions+=1;
+        tKelvin=KelvinVersCelcius(tKelvin);
+        tKelvin=CelciusVersFarenheit(tKelvin);
+        return tKelvin;
     }
-    
-    Scanner sc = new Scanner(System.in);
-    System.out.println ("saisssissez la temperature");
-    temp  = sc.nextDouble();
-    System.out.println ("Saisissez la conversion que vous souhaiter effectuer : \n 1|De Celcius vers Kelvin \n 2| De Kelvin vers Celcius \n 3| De Farenheit vers Celcius \n 4| De Celcius vers Farenheit \n 5| De Kelvin vers Farenheit \n 6| De Farenheit vers Kelvin  ");
-    type  = sc.nextInt();
-    if (type == 1){
-        C2K = CelciusVersKelvin(temp);
-        System.out.println("la temperature en Kelvin est:" + C2K);
-    }
-    if (type== 2){
-        C2K = KelvinVersCelcius(temp);
-        System.out.println("la temperature en Celcius est:" + C2K);
-    }
-    if (type== 3){
-        C2K = FarenheitVersCelcius(temp);
-        System.out.println("la temperature en Celcius est:" + C2K);
-    }
-    if (type== 4){
-        C2K = CelciusVersFarenheit(temp);
-        System.out.println("la temperature en Kelvin est:" + C2K);
-    }
-    if (type== 5){
-       C2K = KelvinVersFarenheit(temp);
-        System.out.println("la temperature en Farenheit est:" + C2K);
-    }
-    if (type== 6){
-        C2K = FarenheitVersKelvin(temp);
-        System.out.println("la temperature en Kelvin est:" + C2K);
+    public double FarenheitVersKelvin (double tFar){
+        nbConversions+=1;
+        tFar = FarenheitVersCelcius(tFar);
+        tFar = CelciusVersKelvin(tFar);
+        return tFar;
     }
     @Override
-    public String toString() {
-        return "nb de conversions =" + nbConversions;
+    public String toString (){
+        return "nb de conversions"+ nbConversions;
+       
     }
 }
+
