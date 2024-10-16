1ef115444bb5d70db8db589db156e702e77c5d9e
Damien Grebil <damien.grebil@epfedu.fr>
2024-10-16 11:11:18 +0200

    TP2 exo 2

---  TP2_manip_Grebil/src/tp2_manip_grebil/TP2_manip_Grebil.java
+++  TP2_manip_Grebil/src/tp2_manip_grebil/TP2_manip_Grebil.java
@@ -16,7 +16,6 @@ public class TP2_manip_Grebil {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-    int nbCalories =0;
         Tartiflette assiette1 = new Tartiflette(500) ; 
         Tartiflette assiette2 = new Tartiflette(600) ;
         Tartiflette assiette3 = assiette2;  // Assiette 2 et 3 referencent la meme tartiflette
@@ -25,9 +24,16 @@ public class TP2_manip_Grebil {
         System.out.println("nb de calories de Assiette 2 : " + assiette2.nbCalories) ; 
         System.out.println("nb de calories de Assiette 3 : " + assiette3.nbCalories) ; 
     // Question six, non cela ne fonctionne pas car l'un ne peux pas etre convertit en l'autre.
-        for( int i= 0; i<= 10;i++) {
-            Moussaka[] tableauMoussaka = new Moussaka[i];
-            tableauMoussaka[i] = Moussaka(100);
-        }
+        Moussaka[] tableauMouss = new Moussaka[10];
+        tableauMouss[1] = Moussaka(100);
+        tableauMouss[2] = Moussaka(120);
+        tableauMouss[3] = Moussaka(150);
+        tableauMouss[4] = Moussaka(160);
+        tableauMouss[5] = Moussaka(300);
+        tableauMouss[6] = Moussaka(450);
+        tableauMouss[7] = Moussaka(230);
+        tableauMouss[8] = Moussaka(540);
+        tableauMouss[9] = Moussaka(240);
+        tableauMouss[10] = Moussaka(500);
     }
 }
