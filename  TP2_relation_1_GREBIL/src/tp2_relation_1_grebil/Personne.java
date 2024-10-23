c9aceaeb70e57a9b592a5c8f9ab1e6e320c7d93e
Damien Grebil <damien.grebil@epfedu.fr>
2024-10-23 10:49:00 +0200

    TP2 exo 4

---  TP2_relation_1_GREBIL/src/tp2_relation_1_grebil/Personne.java
+++  TP2_relation_1_GREBIL/src/tp2_relation_1_grebil/Personne.java
@@ -12,17 +12,18 @@ package tp2_relation_1_grebil;
 public class Personne {
     String nom;
     String prenom;
-    Voiture[] liste_voiture;
+    int nb_Voitures;
+    Voiture[] liste_voitures;
     public Personne(String UnNom, String UnPrenom, Voiture[] Vagos){
         nom = UnNom;
         prenom = UnPrenom;
-        liste_voiture = Vagos;
-        
+        liste_voitures = new Voiture[3];
+        nb_Voitures= 0;
     }
 
     @Override
     public String toString() {
-        return "Personne{" + "nom=" + nom + ", prenom=" + prenom + ", Vagos=" + liste_voiture + '}';
+        return "Personne{" + "nom=" + nom + ", prenom=" + prenom + ", Vagos=" + liste_voitures + '}';
     }
         
 }
