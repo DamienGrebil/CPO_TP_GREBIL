bd35e12567883080a1b05a122de9e45f4a8ddcf8
Damien Grebil <damien.grebil@epfedu.fr>
2024-10-23 11:16:35 +0200

    TP2 exo 4

---  TP2_relation_1_GREBIL/src/tp2_relation_1_grebil/Personne.java
+++  TP2_relation_1_GREBIL/src/tp2_relation_1_grebil/Personne.java
@@ -20,6 +20,26 @@ public class Personne {
         liste_voitures = new Voiture[3];
         nb_Voitures= 0;
     }
+    
+    public boolean ajouter_voiture( Voiture voiture_a_ajouter) { 
+        if (voiture_a_ajouter.proprietaire == null){
+            return false;
+        }
+        if (nb_Voitures > 2){
+            return false;
+        }else{
+            if (nb_Voitures == 0){
+               liste_voitures[0] =  voiture_a_ajouter;
+            }
+            if (nb_Voitures == 1){
+               liste_voitures[1] =  voiture_a_ajouter;
+            }
+            if (nb_Voitures == 2){
+               liste_voitures[2] =  voiture_a_ajouter;
+            }
+        }
+    return true;
+    }
 
     @Override
     public String toString() {
