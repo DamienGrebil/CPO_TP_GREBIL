b1e5c82f2b133b24b1499507efb50daa8abe9b81
Damien Grebil <damien.grebil@epfedu.fr>
2024-10-23 10:51:27 +0200

    TP2 exo 4

---  TP2_relation_1_GREBIL/src/tp2_relation_1_grebil/TP2_relation_1_GREBIL.java
+++  TP2_relation_1_GREBIL/src/tp2_relation_1_grebil/TP2_relation_1_GREBIL.java
@@ -20,8 +20,12 @@ public class TP2_relation_1_GREBIL {
         Voiture une2008 = new Voiture ("2008", "Peugeot", 6 ) ; 
         Voiture uneMicra = new Voiture ("Micra", "Nissan", 4 ) ; 
         Personne bob = new Personne("Bobby", "Sixkiller"); 
-        Personne reno = new Personne("Reno", "Raines"); 
+        Personne reno = new Personne("Reno", "Raines");  
         System.out.println("liste des voitures disponibles: " + "\n" + uneClio  + "\n" + uneAutreClio   + "\n" + une2008   + "\n" + uneMicra ) ;
+        bob.liste_voitures[0] = uneClio ; 
+        bob.nbVoitures = 1 ; 
+        uneClio.proprietaire = Bob ;
+        System.out.println("la premiere voiture de Bob est " + liste_voitures[0] ) ; 
     }
     
 }
