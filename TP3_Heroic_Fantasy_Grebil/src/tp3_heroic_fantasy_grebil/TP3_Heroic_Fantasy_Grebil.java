d41816af656812e4328e8f4f5e35244eb5276305
Damien Grebil <damien.grebil@epfedu.fr>
2024-11-13 16:39:11 +0100

    TP3 classe arme

--- TP3_Heroic_Fantasy_Grebil/src/tp3_heroic_fantasy_grebil/TP3_Heroic_Fantasy_Grebil.java
+++ TP3_Heroic_Fantasy_Grebil/src/tp3_heroic_fantasy_grebil/TP3_Heroic_Fantasy_Grebil.java
@@ -15,7 +15,10 @@ public class TP3_Heroic_Fantasy_Grebil {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Epee Exca = new Epee(7,"Exalibur",5);
+        Epee Dur = new Epee(4,"Durandal",7);    
+        Baton Chene = new Baton(4,"Chêne",5);
+        Baton Charme = new Baton(5,"Charme",6);
     }
     
 }
