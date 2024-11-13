ccebdbdfde8cf20978b789c7fd5238718d150cd3
Damien Grebil <damien.grebil@epfedu.fr>
2024-11-13 15:56:49 +0100

    TP3 classe arme

--- TP3_Heroic_Fantasy_Grebil/src/tp3_heroic_fantasy_grebil/Arme.java
+++ TP3_Heroic_Fantasy_Grebil/src/tp3_heroic_fantasy_grebil/Arme.java
@@ -1,6 +1,7 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+* TP2 relation 1
+* DamienGrebil
+* Octobre 2024
  */
 package tp3_heroic_fantasy_grebil;
 
